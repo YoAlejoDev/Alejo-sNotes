66e806d719253da55ea48fc18d842bb87f2810dc
nmkahurtado <137195757+nmkahurtado@users.noreply.github.com>
2024-03-03 11:39:09 -0500

    Primer commit

---
+++ javascript/animation.js
@@ -0,0 +1,39 @@
+let sections = document.querySelectorAll('.sec');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
+            console.log("Se ve animation");
+        } else {
+            sec.classList.remove('show-animate');
+            console.log("No se ve animation");
+        }
+    })
+}
+
+const scrollers = document.querySelectorAll('.scroller');
+
+if (!window.matchMedia("(prefers-reduce-motion: reduce)").matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach(s => {
+        s.setAttribute('data-animated', true);
+
+        const scrollerInner = document.querySelector('.scroller_inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach(i => {
+            const duplicatedItem = i.cloneNode(true);
+            duplicatedItem.setAttribute('aria-hidden', true);
+            scrollerInner.appendChild(duplicatedItem);
+        })
+
+    })
+}
